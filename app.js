e0f27f2ec9c8015f9166ab77c9394392499e3d0e
NISHAN75 <nishandas880@gmail.com>
2021-06-15 22:03:11 +0600

    YoGo website

---
+++ app.js
@@ -0,0 +1,18 @@
+//video area
+
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo"); 
+
+
+ function stopVideo(){
+
+   videoPlayer.style.display = "none";
+
+ }
+
+         
+    function playVideo(file){
+
+      myVideo.src = file;
+      videoPlayer.style.display = "block"
+    }
